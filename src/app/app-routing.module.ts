cc139f98fd2bea42892c6ba8ef745f2d0053ae0a
KaykyRich <kayky.richard55@gmail.com>
2024-06-01 01:12:43 -0300

    Refatoração das telas com Upload de Video e utilização do PrimeNG

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { VideoManagerComponent } from './Components/video-manager/video-manager.component';
+import { VideoInputComponent } from './views/video-input/video-input.component';
+
+const routes: Routes = [
+  {path: '', component: VideoInputComponent},
+  {path: 'upload', component: VideoManagerComponent},
+];
 
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,38 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
+import { CardModule } from 'primeng/card';
+import {StyleClassModule} from 'primeng/styleclass';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { UploadVideoComponent } from './Components/upload-video/upload-video.component';
 import { VideoManagerComponent } from './Components/video-manager/video-manager.component';
+import { VideoInputComponent } from './views/video-input/video-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     FooterComponent,
     UploadVideoComponent,
-    VideoManagerComponent
+    VideoManagerComponent,
+    VideoInputComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    StyleClassModule,
+    CardModule,
     HttpClientModule,
     AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
